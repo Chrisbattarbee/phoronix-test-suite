3cf701e1c8d67d486deedcd5f1140384b6d17c1f
Chris Battarbee <chris@chrisbattarbee.com>
2020-05-30 19:27:27 +0100

    Update env

--- env.sh
+++ env.sh
@@ -2,3 +2,8 @@
 # Make sure to set PROFDATATOOL
 
 export PTS_USER_PATH_OVERRIDE=/data/.phoronix-test-suite
+export CC=/data/llvm-project/llvm/build-release/bin/clang
+export CXX=/data/llvm-project/llvm/build-release/bin/clang++
+export PROFDATATOOL=/data/llvm-project/llvm/build-release/bin/llvm-profdata
+alias cc=/data/llvm-project/llvm/build-release/bin/clang
+
